8f3d2d2e6c41d3a3ef89453ef1891a5148ad918d
liuwenjie <liuwenjie@goumin.com>
2016-03-08 19:36:11 +0800

    adviceutil

---
+++ library/app/src/main/java/com/lwj/utils/UtilException.java
@@ -0,0 +1,12 @@
+package com.lwj.utils;
+
+/**
+ * Created by lwj on 2016/3/8.
+ * liuwenjie@goumin.com
+ */
+public class UtilException extends Exception {
+
+    public UtilException(String detailMessage) {
+        super(detailMessage);
+    }
+}
--- library/app/src/main/java/com/lwj/utils/ViewUtil.java
+++ library/app/src/main/java/com/lwj/utils/ViewUtil.java
@@ -1,5 +1,9 @@
 package com.lwj.utils;
 
+import android.app.Activity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 
 /**
@@ -8,19 +12,56 @@ import android.widget.ListView;
  */
 public class ViewUtil {
 
+    public static <V extends View> V find(Activity ac, int id) {
+        return (V) ac.findViewById(id);
+    }
 
-    public static <T> T getItemModel(ListView listView,int i){
+    public static <V extends View> V find(View view, int id) {
+        return (V) view.findViewById(id);
+    }
 
-        if(listView == null){
+    public static void viewGone(View _view) {
+        if (_view != null && _view.getVisibility() != View.GONE) {
+            _view.setVisibility(View.GONE);
+        }
+
+    }
+
+    public static void viewVisible(View _view) {
+        if (_view != null && _view.getVisibility() != View.VISIBLE) {
+            _view.setVisibility(View.VISIBLE);
+        }
+
+    }
+
+    public static <T> T getItemModel(ListView listView, int i) {
+
+        if (listView == null) {
             return null;
         }
-        if(i < 0){
-            return  null;
+        if (i < 0) {
+            return null;
         }
         int headerCount = listView.getHeaderViewsCount();
         if (i >= headerCount) {
-            return  (T) listView.getAdapter().getItem(i);
+            return (T) listView.getAdapter().getItem(i);
         }
         return null;
+    }
+    public static void setListViewHeightBasedOnChildren(ListView listView) {
+        ListAdapter listAdapter = listView.getAdapter();
+        if(listAdapter != null) {
+            int totalHeight = 0;
+
+            for(int params = 0; params < listAdapter.getCount(); ++params) {
+                View listItem = listAdapter.getView(params, (View)null, listView);
+                listItem.measure(0, 0);
+                totalHeight += listItem.getMeasuredHeight();
+            }
+
+            ViewGroup.LayoutParams var5 = listView.getLayoutParams();
+            var5.height = totalHeight + listView.getDividerHeight() * (listAdapter.getCount() - 1);
+            listView.setLayoutParams(var5);
+        }
     }
 }
