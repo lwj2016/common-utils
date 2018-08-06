3001d606158cddc3cca4c3f17caf63baace3d4e9
lwjfork <lwjfork@gmail.com>
2018-08-06 11:41:34 +0800

    反射工具类

---
+++ AndroidUtils/src/main/java/com/lwj/utils/ReflectUtil.java
@@ -0,0 +1,27 @@
+package com.lwj.utils;
+
+import java.lang.reflect.Method;
+
+/**
+ * Created by lwj on 2018/8/3.
+ * lwjfork@gmail.com
+ */
+
+public class ReflectUtil {
+
+
+    public static Method[] getDeclaredMethods(Class clazz) {
+        return clazz.getDeclaredMethods();
+    }
+
+    public static Method[] getPublicMethods(Class clazz) {
+        return clazz.getMethods();
+    }
+
+    @SuppressWarnings("unchecked")
+    public static Method getMethod(String methodName, Class clazz, Class<?>... parameterTypes) throws NoSuchMethodException {
+        return clazz.getMethod(methodName, parameterTypes);
+    }
+
+
+}
